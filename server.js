9666e1b92667b00fbaddd18d9dcbb2bd1062d1ec
Suronjit pal <suronjit797@gmail.com>
2022-04-25 21:28:36 +0600

    todo

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 app.get('/', (req, res) => {
-    res.json({ server: 'server is online' })
+    res.json({ server: ' todo server is online' })
 })
 
 
